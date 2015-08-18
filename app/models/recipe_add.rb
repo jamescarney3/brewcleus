bbd03a0b29a659eff167d57ca0bac53a861358d6
James Carney <jmcarneyiii@gmail.com>
2015-08-18 17:35:12 -0400

    Implement validations for recipe_adds

--- app/models/recipe_add.rb
+++ app/models/recipe_add.rb
@@ -1,5 +1,7 @@
 class RecipeAdd < ActiveRecord::Base
   validates :user_id, :recipe_id, presence: true
+  validate :user_is_not_author
+  validate :not_already_followed
 
   belongs_to(
     :user,
@@ -14,5 +16,15 @@ class RecipeAdd < ActiveRecord::Base
     foreign_key: :recipe_id,
     primary_key: :id
   )
-  
+
+  private
+
+  def user_is_not_author
+    Recipe.find(self.recipe_id).author != User.find(self.user_id)
+  end
+
+  def not_already_followed
+    User.find(self.user_id).added_recipes.include?(Recipe.find(self.recipe_id))
+  end
+
 end
