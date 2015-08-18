d0bb8731df7db1444270f5fb996914e598be379a
James Carney <jmcarneyiii@gmail.com>
2015-08-17 22:06:49 -0400

    Add sign-in/out buttons to header, make header listen to currentUser

--- app/assets/javascripts/views/shared/header.js
+++ app/assets/javascripts/views/shared/header.js
@@ -4,8 +4,23 @@ Brewcleus.Views.Header = Backbone.View.extend({
 
   template: JST["shared/header"],
 
+  events: {
+    "click #sign-out-link" : "signOut"
+  },
+
   initialize: function(options){
+    this.listenTo(Brewcleus.currentUser, "signIn signOut change", this.render);
     this.render();
+  },
+
+  signOut: function(event){
+    event.preventDefault();
+
+    Brewcleus.currentUser.signOut({
+      success: function(){
+        Backbone.history.navigate("", { trigger: true });
+      }
+    });
   },
 
   render: function(){
